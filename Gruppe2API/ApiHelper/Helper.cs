610638aa1866c71a1a137c99fad5b0f0f983de31
jobl0001 <jobl0001@edu.mercantec.dk>
2023-06-28 12:23:11 +0200

    You can now get Stadium with its matches -Sections to come

---
+++ Gruppe2API/ApiHelper/Helper.cs
@@ -0,0 +1,105 @@
+﻿using Models;
+using MySql.Data.MySqlClient;
+using static System.Collections.Specialized.BitVector32;
+using System.Data;
+
+namespace ApiHelper
+{
+    public class Helper
+    {
+        string connectionString;
+
+        // Other methods and properties...
+
+        public Helper()
+        {
+            connectionString = "Server=sql7.freemysqlhosting.net; User ID=sql7629046; Password=23deIfAcse; Database=sql7629046";
+        }
+
+        private Stadium MapStadium(IDataReader reader)
+        {
+            var stadium = new Stadium
+            {
+                Id = Convert.ToInt32(reader["Id"]),
+                Name = Convert.ToString(reader["Name"]),
+                MatchList = new List<Match>()
+            };
+
+            return stadium;
+        }
+
+        private Match MapMatch(IDataReader reader)
+        {
+            var match = new Match
+            {
+                MatchId = Convert.ToInt32(reader["MatchId"]),
+                SoldSeats = Convert.ToInt32(reader["SoldSeats"]),
+                MatchDate = Convert.ToDateTime(reader["MatchDate"]),
+                HomeTeam = Convert.ToString(reader["HomeTeam"]),
+                AwayTeam = Convert.ToString(reader["AwayTeam"]),
+
+            };
+
+            return match;
+        }
+
+
+        public List<Match> GetMatches(int stadiumId)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                var query = $"SELECT * FROM MatchInfo WHERE StadionId = {stadiumId}";
+                List<Match> matches = new List<Match>();
+                var command = new MySqlCommand(query, connection);
+
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    matches.Add(MapMatch(reader));
+
+                }
+
+                return matches;
+            }
+        }
+
+
+        public Stadium GetStadium(int stadiumId)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                var query = $"SELECT * FROM Stadion WHERE Id = {stadiumId}";
+                Stadium stadium = null;
+                var command = new MySqlCommand(query, connection);
+
+                connection.Open();
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    if (stadium == null)
+                    {
+                        stadium = MapStadium(reader);
+                    }
+
+                   
+                }
+
+                if (stadium != null)
+                {
+                    stadium.MatchList = GetMatches(stadium.Id);
+                }
+
+                return stadium;
+            }
+        }
+
+
+
+    }
+}
---
+++ Gruppe2API/Models/Match.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Match
+    {
+        public int MatchId { get; set; }
+
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+
+        public DateTime MatchDate { get; set; }
+
+        public int SoldSeats { get; set; }
+    }
+}
