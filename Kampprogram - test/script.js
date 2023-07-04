7431cbf0752d34a58a41448d575c1076af5e5ef9
KrokoChicken <127853987+KrokoChicken@users.noreply.github.com>
2023-07-04 20:03:53 +0200

    Kamprogram side

--- Kampprogram - test/script.js
+++ Kampprogram - test/script.js
@@ -1,11 +1,46 @@
-function createDivElements() {
-    var container = document.getElementById('container');
+let bool = true;
 
-    for (var i = 0; i < 5; i++) {
-        var div = document.createElement('div');
-        div.className = 'box';
-        div.textContent = "Arena + Hold + Tidspunkt"
+async function getMatches() {
+  let container = document.getElementById('container');
+  
+  
+  while(bool) {
+    try {
+      const response = await fetch('https://localhost:7085/Stadium/GetStadium?stadiumId=1');
+      const data = await response.json();
+      
+      const arrayLength = data.matchList.length;
 
-        container.appendChild(div);
-    }
-}
+      for (var i = 0; i < arrayLength; i++) {
+          const homeTeam = data.matchList[i].homeTeam;
+          const awayTeam = data.matchList[i].awayTeam;
+          const matchDate = data.matchList[i].matchDate;
+
+          let div = document.createElement('div');
+          let button = document.createElement('button');
+          button.id = 'myButton';
+          button.textContent = 'Køb billeter';
+            button.onclick = () => {
+  
+            };
+          
+          div.className = 'box';
+          div.textContent = homeTeam + " mod " + awayTeam + " " + matchDate;
+
+          container.appendChild(div);
+          div.appendChild(button);
+          
+          bool = false;
+      }
+    } catch (error) {
+        console.log('Error:', error);
+      }
+  }
+};
+
+
+
+
+
+
+  
