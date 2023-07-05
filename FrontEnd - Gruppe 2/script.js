19d6e9a3091a221e1b8fbc95a9f77c7768226811
DNot3 <Kjetil_t@hotmail.com>
2023-07-05 11:42:01 +0200

    s

--- FrontEnd - Gruppe 2/script.js
+++ FrontEnd - Gruppe 2/script.js
@@ -1,3 +1,16 @@
 const homebutton = document.querySelector(".home home1");
 
 
+var link = ""
+function getData(){
+    var xhttp = new XMLHttpRequest ();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.response);
+            document.getElementById("title").innerHTML = obj.Title;
+        }
+    };
+    xhttp.open("GET", link, true);
+    xhttp.send();
+}
+getData();
